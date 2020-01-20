7d5eb80fbb532be40918083000700ff9ac0d3d2e
Kevinjj30 <kevinjenkins56@yahoo.com>
2020-01-19 19:06:20 -0600

    Done I think

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     this.state = {
     news:[],
     SearchAny:"",
-    SearchDates: "",
+    searchDates: "",
   }
 }
 
@@ -30,13 +30,14 @@ class App extends React.Component {
   handleSubmitDate =(e) => {
     // always put this line in on submits, it prevents the page from reloading and wiping your state
     e.preventDefault();
-    this.fetchDate(this.state.SearchDates)
+    this.fetchDates(this.state.searchDates)
     // after doing something with the data we reset the form value to empty quotes again
     this.setState({value: ''})
   }
+//All the handle options I pulled straight from the prework and modified with the fetch functions
 
-  fetchAny = () => {
-    fetch (`http://hn.algolia.com/api/v1/search?query=&tags=story`)
+  fetchAny = (search) => {
+    fetch (`http://hn.algolia.com/api/v1/search?query=${search}&tags=story`)
     .then(response => response.json())
     .then(data => {
       if (data.hits.length > 0) {
@@ -47,9 +48,10 @@ class App extends React.Component {
     })
     .catch(error => console.log("Parsing failed: ", error))
   }
+// I saw a video on setting up my fetches this syntax was a little different for my beer app but this works well with less lines.
 
-    fetchAuthor = () => {
-      fetch (`http://hn.algolia.com/api/v1/search_by_date?query=$tags=story`)
+    fetchDates = (search) => {
+      fetch (`http://hn.algolia.com/api/v1/search_by_date?query=${search}&tags=story`)
       .then(response => response.json())
       .then(data => {
         if (data.hits.length > 0) {
@@ -80,29 +82,32 @@ class App extends React.Component {
           </form>
           <form>
             <input
-            type = 'text'
-            name= 'SearchDates'
+            type = 'date'
+            name= 'searchDates'
             placeholder="Search by Dates"
-            value={this.state.SearchDates}
+            value={this.state.searchDates}
             onChange={this.handleChange}></input>
-            <button onclick={this.handleSubmitDate}>Search</button>
+            <button onClick={this.handleSubmitDate}>Search</button>
           </form>
           </div>
           <div className="results-show">
           {this.state.news.map ((news, index) => (
             <div>
-              <div key={index}>
-                <p><a href={news.url}>{news.title}</a> by {news.dates}</p>
+              <div key={index+1}>
+                <p><a href={news.url}>{news.title}</a></p>
+                <p>{news.story_text}</p>
                 <p>Published: {news.created_at}</p>
               </div>
             </div>
           ))}
         </div>
           </div>
-      );
+      )
           }
   }
-    
+  // Finally got my render to work I got held up literally 30minutes on this part cause I had "onclick" instead of "onClick"..... so yea haha....also forgot to change my type to date for the input on the dates fetch.
   
 
 export default App;
+
+
