b6ce16aeb38bc255884383512c8432e83dec5370
Kevinjj30 <kevinjenkins56@yahoo.com>
2020-01-20 17:13:40 -0600

    done

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
 
     render() {
       return (
-        <div className= "bodypage">
+        <div className= "bodypaget">
         <div className="formbox">
           <form>
             <input
